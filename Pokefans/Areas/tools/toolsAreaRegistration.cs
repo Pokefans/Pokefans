d61e8ababcc3a4930b90ca049923d07c59fd7381
Matthias Bogad <matthias@bogad.at>
2015-05-21 23:45:07 +0200

    Added tools Area with default route

---
+++ Pokefans/Areas/tools/toolsAreaRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using Pokefans.Util;
+
+namespace Pokefans.Areas.tools
+{
+    public class toolsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "tools";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.Routes.Add(new DomainRoute(
+                "user." + ConfigurationManager.AppSettings["Domain"],
+                "{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+                )
+            );
+        }
+    }
+}
