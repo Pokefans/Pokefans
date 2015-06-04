d1d4ecfdd2d4c44aa2d2ebddf84c5317d067c0b6
Matthias Bogad <matthias@bogad.at>
2015-06-05 01:12:05 +0200

    Added empty management area

---
+++ Pokefans/Areas/mitarbeit/mitarbeitAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using Pokefans.Util;
+
+namespace Pokefans.Areas.mitarbeit
+{
+    public class mitarbeitAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "mitarbeit";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.Routes.Add(new DomainRoute(
+                "mitarbeit." + ConfigurationManager.AppSettings["Domain"],
+                "{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional })
+            );
+        }
+    }
+}
