37bfdb8660324145e61759b10395b12e7e8c6fd9
Katharina Bogad <bogad@in.tum.de>
2017-12-26 12:31:03 +0100

    Added forum area

---
+++ Pokefans/Areas/forum/forumAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Pokefans.Areas.forum
+{
+    public class forumAreaRegistration : AreaRegistration
+    {
+		public override string AreaName
+		{
+			get
+			{
+				return "fanart";
+			}
+		}
+
+        public override void RegisterArea(AreaRegistrationContext context) {
+
+
+
+        }
+
+		public forumAreaRegistration()
+        {
+        }
+    }
+}
