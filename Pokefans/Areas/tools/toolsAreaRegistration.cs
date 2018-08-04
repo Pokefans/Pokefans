dcd1de2fc5e02b739075970ca506fa32f43358ab
Katharina Bogad <katharina@hacked.xyz>
2018-08-04 22:52:28 +0200

    Tools: fix area registration

--- Pokefans/Areas/tools/toolsAreaRegistration.cs
+++ Pokefans/Areas/tools/toolsAreaRegistration.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright 2015 the pokefans-core authors. See copying.md for legal info.
 using System.Configuration;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Pokefans.Util;
 
 namespace Pokefans.Areas.tools
@@ -17,10 +18,15 @@ namespace Pokefans.Areas.tools
 
         public override void RegisterArea(AreaRegistrationContext context) 
         {
+            RouteValueDictionary dataTokens = new RouteValueDictionary();
+            dataTokens["Namespaces"] = new string[] { "Pokefans.Areas.tools.Controllers" };
+            dataTokens["UseNamespaceFallback"] = false;
+
             context.Routes.Add(new DomainRoute(
                 "tools." + ConfigurationManager.AppSettings["Domain"],
                 "{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                dataTokens
                 )
             );
         }
